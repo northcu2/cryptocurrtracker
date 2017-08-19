f90c141e865609a7f21cd70d6cfc6c8aaa918ab7
Eddie Northcutt <northcu2@illinois.edu>
2017-08-19 18:25:57 -0500

    Wired up connector and sender. needs to be tested still.

--- src/main/java/com/northcutt/quotetracker/backend/DatabaseConnector.java
+++ src/main/java/com/northcutt/quotetracker/backend/DatabaseConnector.java
@@ -1,27 +1,47 @@
 package com.northcutt.quotetracker.backend;
 
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 public class DatabaseConnector {
-    private Logger logger;
-    public static Connection getConnection(){
-        Connection con = null;
 
+
+
+
+
+
+
+    public static Connection getConnection(){
+        loadDriver();
+
+        Connection con = null;
+        try{
+            con = DriverManager.getConnection(
+                    "jdbc:mysql://aa1jd6l662dtig2.crawqsxgq3hl.us-east-2.rds.amazonaws.com:3306",
+                    "northcutted",
+                    "Jvillxap2");
+            return con;
+
+        }catch (SQLException e){
+                e.printStackTrace();
+                return null;
+        }
     }
 
     private static void loadDriver(){
         try {
-            // The newInstance() call is a work around for some
-            // broken Java implementations
-
             Class.forName("com.mysql.jdbc.Driver").newInstance();
+            System.out.println("Driver Registered!");
         } catch (Exception ex) {
             ex.printStackTrace();
             System.out.println("Could not load JDBC driver.");
         }
     }
+
+
 }
 
